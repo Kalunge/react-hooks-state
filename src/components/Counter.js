b00b459b69e58f734174fe0a67c24cd090451269
kalunge <muthomititus+github@gmail.com>
2022-08-17 10:14:45 +0300

    Update state on click

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 function Counter() {
   const [count, setCount] = useState(0);
 
   function increment() {
-    setCount(count + 1);
+    setCount((count) => count + 1);
   }
 
   return <button onClick={increment}>I have been clicked {count} times</button>;
